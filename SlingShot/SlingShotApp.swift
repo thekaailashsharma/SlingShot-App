a9e39d2933a6bd83bbce96616871cbdeb7c12844
Kailash Sharma <kailashps.1011@gmail.com>
2024-03-24 20:23:40 +0530

    Initial Commit

---
+++ SlingShot/SlingShotApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlingShotApp.swift
+//  SlingShot
+//
+//  Created by Kailash on 24/03/24.
+//
+
+import SwiftUI
+
+@main
+struct SlingShotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
